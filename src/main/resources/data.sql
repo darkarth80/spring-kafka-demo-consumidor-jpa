48b107fd130ecc1ef44b782e77ddc5b7b2dc9f25
darkarth80 <art.cue.vaz@gmail.com>
2019-10-29 12:33:15 -0600

    Se elimina archivo sql.

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE registro (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    contenido NVARCHAR(3500) NOT NULL,
-    id_aplicacion NVARCHAR(6) NOT NULL,
-    fecha_registro DATETIME NOT NULL
-);
