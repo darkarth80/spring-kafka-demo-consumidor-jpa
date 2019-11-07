99e3afc4bb4e26310a4346b0735233cb9b211b43
darkarth80 <26891629+darkarth80@users.noreply.github.com>
2019-11-07 16:05:02 -0600

    Se habilita la base como servidor de bd.

--- src/main/java/com/darkarth/kafka/demo/SpringKafkaDemoConsumidorJpaApplication.java
+++ src/main/java/com/darkarth/kafka/demo/SpringKafkaDemoConsumidorJpaApplication.java
@@ -1,7 +1,11 @@
 package com.darkarth.kafka.demo;
 
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringKafkaDemoConsumidorJpaApplication {
@@ -9,5 +13,16 @@ public class SpringKafkaDemoConsumidorJpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringKafkaDemoConsumidorJpaApplication.class, args);
 	}
+
+	/**
+     * Start internal H2 server so we can query the DB from IDE
+     *
+     * @return H2 Server instance
+     * @throws SQLException
+     */
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "8081");
+    }
 
 }
--- src/test/java/com/darkarth/kafka/demo/KafkaConsumerDbApplicationTests.java
+++ src/test/java/com/darkarth/kafka/demo/KafkaConsumerDbApplicationTests.java
@@ -1,6 +1,6 @@
 package com.darkarth.kafka.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +8,11 @@ class KafkaConsumerDbApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		String mens = "lkaslksalfdasfdsafadsfdsafdasfdskasd applicationId=ASDASD";
+		int lastIndex = mens.indexOf("applicationId=")+14;
+		System.out.println("applicationId:" + mens.substring(lastIndex, lastIndex+6));
+		String applicationId = "";
 	}
 
 }
